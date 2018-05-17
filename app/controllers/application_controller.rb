d0032f0c0ef5f64e940f7e95c894162921ccc216
Silvia Abreu <silvia.abreu@wolox.cl>
2018-05-17 16:41:24 -0400

    configuration of Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:create) {|u| u.permit({:email, :password, :password_confirmation, :first_name, :last_name)}}
+  end
+
 end
---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+	validates_presence_of :first_name
+	validates_presence_of :last_name
+
+ 	 # Include default devise modules. Others available are:
+ 	 # :lockable, :timeoutable and :omniauthable
+ 	 devise :database_authenticatable, :registerable,
+ 	        :recoverable, :rememberable, :trackable, :validatable
+
+
+end
