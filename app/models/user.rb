9f8da7714aff5c14231311431551ca581526e7bf
Silvia Abreu <silvia.abreu@wolox.cl>
2018-05-18 16:43:32 -0400

    set devise_key

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable,
          :rememberable, :trackable, :validatable, :omniauthable
   include DeviseTokenAuth::Concerns::User
-	attr_accessor :login
-    validates :first_name, presence: true
-    validates :last_name, presence: true
+  attr_accessor :login
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
