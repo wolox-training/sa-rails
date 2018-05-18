507ddd73b072616fa7ce692ef3b5626792a8b451
Silvia Abreu <silvia.abreu@wolox.cl>
2018-05-18 16:18:36 -0400

    authentication with devise_auth_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable, :recoverable,
+         :rememberable, :trackable, :validatable, :omniauthable
+  include DeviseTokenAuth::Concerns::User
+	attr_accessor :login
+    validates :first_name, presence: true
+    validates :last_name, presence: true
 end
