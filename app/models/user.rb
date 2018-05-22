0213bd08d1a8b98140f53662b8d4ad646adfae8a
Silvia Abreu <silvia.abreu@wolox.cl>
2018-05-22 16:30:25 -0400

    requested changes in user.rb and application_controller.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules.
+  validates :first_name, :last_name, presence: true
+
   devise :database_authenticatable, :registerable, :recoverable,
          :rememberable, :trackable, :validatable, :omniauthable
+
   include DeviseTokenAuth::Concerns::User
-  attr_accessor :login
-  validates :first_name, presence: true
-  validates :last_name, presence: true
 end
--- config/application.rb
+++ config/application.rb
@@ -10,23 +10,6 @@ module WBooks
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    INITIAL_VARIABLES = [:secrets]
-
-    def secrets #:nodoc:
-      @secrets ||= begin
-      	secrets = ActiveSupport::OrderedOptions.new
-        yaml = config.paths["config/secrets"].first
-
-        if File.exist?(yaml)
-          require "erb"
-          all_secrets = YAML.load(ERB.new(IO.read(yaml)).result) || {}
-          env_secrets = all_secrets[Rails.env]
-          secrets.merge!(env_secrets.symbolize_keys) if env_secrets
-        end
-        secrets.secret_mail_base ||= config.secret_mail_base
-        secrets
-      end
-    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
