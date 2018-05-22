bb74a38ac46a817f9ae801c9e6e6a62003062ae5
Silvia Abreu <silvia.abreu@wolox.cl>
2018-05-22 15:35:14 -0400

    unused controllers and commented lines of code are removed

--- app/controllers/users/sessions_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Users
-  class SessionsController < Devise::SessionsController
-    devise_for :users, controllers: { sessions: 'users/sessions' }
-  end
-end
