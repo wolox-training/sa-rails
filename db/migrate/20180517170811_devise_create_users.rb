02e257d7dc008b858a0bcf0b9109a50c28ddb886
Silvia Abreu <silvia.abreu@wolox.cl>
2018-05-23 10:00:27 -0400

    change of notation from '=>' to ':'

--- db/migrate/20180517170811_devise_create_users.rb
+++ db/migrate/20180517170811_devise_create_users.rb
@@ -4,8 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table(:users) do |t|
       ## Required
-      t.string :provider, :null => false, :default => "email"
-      t.string :uid, :null => false, :default => ""
+      t.string :provider, null: false, default: "email"
+      t.string :uid, null: false, default: ""
 
       ## Database authenticatable
       t.string :email, null: false, default: ""
@@ -16,13 +16,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
-      t.boolean  :allow_password_change, :default => false
+      t.boolean  :allow_password_change, default: false
 
       ## Rememberable
       t.datetime :remember_created_at
 
       ## Trackable
-      t.integer  :sign_in_count, :default => 0, :null => false
+      t.integer  :sign_in_count, default: 0, null: false
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
       t.string   :current_sign_in_ip
