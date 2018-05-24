9586c11c68f5db579b0fef858e347ca10fca9370
Silvia Abreu <silvia.abreu@wolox.cl>
2018-05-24 10:48:09 -0400

    creation of spec dir and factories for book

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe Book, type: :model do
@@ -8,14 +10,14 @@ describe Book, type: :model do
   it { should validate_presence_of(:publisher) }
   it { should validate_presence_of(:year) }
 
-  subject(:book) { create(:book) } 
+  subject(:book) { create(:book) }
 
   it { is_expected.to be_valid }
 
   describe '#generate_verification_code' do
     context 'When the book is created' do
       it 'generates a random verification code' do
-        book.save! 
+        book.save!
       end
     end
   end
