aa02d267a9e65e3006822ef4e84f37aff2a4dffc
Silvia Abreu <silvia.abreu@wolox.cl>
2018-05-25 17:15:50 -0400

    routes for book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Book < ApplicationRecord
+  paginates_per 50
   validates :genre, :author, :image, :title, :publisher, :year, presence: true
 
   has_many :rents, dependent: :destroy
