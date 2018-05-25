6f5539949fde7c117608da861e254b7d01618cf1
Silvia Abreu <silvia.abreu@wolox.cl>
2018-05-25 09:44:23 -0400

    suggested changes to code review

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,6 +3,9 @@
 require 'rails_helper'
 
 describe Book, type: :model do
+
+  subject(:book) { create(:book) }
+
   it { should validate_presence_of(:genre) }
   it { should validate_presence_of(:author) }
   it { should validate_presence_of(:image) }
@@ -10,15 +13,5 @@ describe Book, type: :model do
   it { should validate_presence_of(:publisher) }
   it { should validate_presence_of(:year) }
 
-  subject(:book) { create(:book) }
-
   it { is_expected.to be_valid }
-
-  describe '#generate_verification_code' do
-    context 'When the book is created' do
-      it 'generates a random verification code' do
-        book.save!
-      end
-    end
-  end
 end
