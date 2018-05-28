fa420299b3ebd347a9ce038221dc4eab0379a081
Silvia Abreu <silvia.abreu@wolox.cl>
2018-05-28 10:48:46 -0400

    Index and Show routes for the Book model are implemented with authentication

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    books = Book.all.page(1)
+    render json: books
+  end
+
+  def show
+    book = Book.find(params[:id])
+    render json: book
+  end
+end
