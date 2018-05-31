6ca48144173e179503182a92986105b0d069920b
Silvia Abreu <silvia.abreu@wolox.cl>
2018-05-31 09:57:23 -0400

    changes in rents factory

--- spec/factories/rents.rb
+++ spec/factories/rents.rb
@@ -2,8 +2,8 @@
 
 FactoryBot.define do
   factory :rent do
-    user_id { create(:user).id }
-    book_id { create(:book).id }
+    user_id { create(:user) }
+    book_id { create(:book) }
     from { Faker::Date.forward(1) }
     to { from.next_day }
   end
