caadf6fbea75d452f81bac5b740e188165275942
Silvia Abreu <silvia.abreu@wolox.cl>
2018-06-05 13:06:43 -0400

    changes for Apiary estandar

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -2,8 +2,9 @@
 
 class RentsController < ApiController
   def index
-    render json: Rent.where(user: params[:user_id]).or(+
-      Rent.where(book: params[:book_id])).page(params[:page])
+    render json: Rent.where(user: params[:user_id]).or(
+      Rent.where(book: params[:book_id])
+    ).page(params[:page])
   end
 
   def create
