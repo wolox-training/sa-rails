7cb5d2d73d7f25d90f1b95e821bd9b01be45ee48
Silvia Abreu <silvia.abreu@wolox.cl>
2018-06-06 10:49:11 -0400

    first steps

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,4 +2,9 @@
 
 class ApiController < ApplicationController
   before_action :authenticate_user!
+  # before_action :set_locale
+#
+#   # def set_locale
+#   #   I18n.locale = current_user.try(:locale) || I18n.default_locale
+  # end
 end
--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module WBooks
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :de
   end
 end
