e9b3edfba31be22ae3dfeb5ecab65df5be41fcb0
Silvia Abreu <silvia.abreu@wolox.cl>
2018-06-08 14:39:34 -0400

    implemented pundit

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,6 +2,13 @@
 
 class ApiController < ApplicationController
   include DeviseTokenAuth::Concerns::SetUserByToken
-  protect_from_forgery with: :null_session
+
   before_action :authenticate_user!
+  before_action :verify_user
+
+  private
+
+  def verify_user
+    authorize User.find(params[:user_id]) if params[:user_id].present?
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include Pundit
+
+  protect_from_forgery with: :null_session
   before_action :set_locale
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   def set_locale
     I18n.locale = current_user.try(:locale) || I18n.default_locale
+  end
+
+  private
+
+  def user_not_authorized
+    render json: { errors: 'You are not authorized for this action.' }, status: :unauthorized
   end
 end
