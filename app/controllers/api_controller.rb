daba70ea293412356a8b7807490c5009ea567a9e
Silvia Abreu <silvia.abreu@wolox.cl>
2018-06-12 16:29:43 -0400

    changes to make it easier read code

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,6 +2,8 @@
 
 class ApiController < ApplicationController
   include DeviseTokenAuth::Concerns::SetUserByToken
+
   protect_from_forgery with: :null_session
+  
   before_action :authenticate_user!
 end
