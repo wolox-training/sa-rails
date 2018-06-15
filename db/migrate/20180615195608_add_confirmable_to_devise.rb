5e2f547578cd90acfd84d7d371d24e2f7d9c87ee
Silvia Abreu <silvia.abreu@wolox.cl>
2018-06-15 17:59:33 -0400

    fix migrate

--- db/migrate/20180615195608_add_confirmable_to_devise.rb
+++ db/migrate/20180615195608_add_confirmable_to_devise.rb
@@ -1,5 +1,5 @@
 class AddConfirmableToDevise < ActiveRecord::Migration[5.2]
-  def up
+  def change
     add_column :users, :confirmation_token, :string
     add_column :users, :confirmed_at, :datetime
     add_column :users, :confirmation_sent_at, :datetime
@@ -7,9 +7,5 @@ class AddConfirmableToDevise < ActiveRecord::Migration[5.2]
     add_index :users, :confirmation_token, unique: true
 
     User.all.update_all confirmed_at: DateTime.now
-  end
-
-  def down
-    remove_columns :users, :confirmation_token, :confirmed_at, :confirmation_sent_at
   end
 end
