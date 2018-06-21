f65b5c72b8ecdc0baff3d1af5b36177f57d05c21
Silvia Abreu <silvia.abreu@wolox.cl>
2018-06-21 17:09:59 -0400

    suggested changes

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class ApiController < ApplicationController
+  include Pundit
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   protect_from_forgery with: :null_session
 
@@ -12,5 +15,9 @@ class ApiController < ApplicationController
 
   def verify_user
     authorize User.find(params[:user_id]) if params[:user_id].present?
+  end
+
+  def user_not_authorized
+    render json: { errors: 'You are not authorized for this action.' }, status: :unauthorized
   end
 end
